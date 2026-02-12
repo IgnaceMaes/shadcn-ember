97a4181646f80e0e93d54b5a2ee5d0e277374237
Ignace Maes <10243652+IgnaceMaes@users.noreply.github.com>
2026-02-12 22:34:35 +0100

    fix: symlink service

--- apps/v4/registry/new-york-v4/services/flash-messages.ts
+++ apps/v4/registry/new-york-v4/services/flash-messages.ts
@@ -1,10 +1,1 @@
-import { FlashMessagesService } from 'ember-cli-flash';
-
-export default class CustomFlashMessagesService extends FlashMessagesService {
-  get flashMessageDefaults() {
-    return {
-      ...super.flashMessageDefaults,
-      type: 'default',
-    };
-  }
-}
+../../../app/services/flash-messages.ts
