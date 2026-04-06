945a6646409506d6eb746746eabfda705544e634
Ignace Maes <10243652+IgnaceMaes@users.noreply.github.com>
2026-04-06 20:31:23 +0200

    fix: lint

--- apps/v4/app/app-ssr.ts
+++ apps/v4/app/app-ssr.ts
@@ -1,8 +1,8 @@
-import EmberApp from 'ember-strict-application-resolver';
 import KeyboardService from 'ember-keyboard/services/keyboard';
 import PageTitleService from 'ember-page-title/services/page-title';
 import GlimmerOverridesInitializer from 'ember-provide-consume-context/initializers/glimmer-overrides';
 import ShikiService from 'ember-shiki/services/shiki';
+import EmberApp from 'ember-strict-application-resolver';
 
 import config from './config/environment.ts';
 import Router from './router.ts';
--- apps/v4/app/entry.ts
+++ apps/v4/app/entry.ts
@@ -1,9 +1,10 @@
+import { shouldRehydrate } from 'vite-ember-ssr/client';
+
 import Application from './app.ts';
 import config from './config/environment.ts';
-import { shouldRehydrate } from 'vite-ember-ssr/client';
 
 const app = Application.create({ ...config.APP, autoboot: false });
 
-app.visit(window.location.pathname + window.location.search, {
+void app.visit(window.location.pathname + window.location.search, {
   ...(shouldRehydrate() ? { _renderMode: 'rehydrate' } : {}),
 });
